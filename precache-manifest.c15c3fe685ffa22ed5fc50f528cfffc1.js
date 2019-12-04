13b9a62884a239ae5a578fe229cc1a1da5df1821
Alexander Sulema <assulema@gmail.com>
2019-12-04 17:27:49 +0200

    Updates

---
+++ precache-manifest.c15c3fe685ffa22ed5fc50f528cfffc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885f21fa18fea00de09782c633f5170f",
+    "url": "/VIA-example/index.html"
+  },
+  {
+    "revision": "4f30271fbfd9cf066338",
+    "url": "/VIA-example/static/css/main.6142b0ca.chunk.css"
+  },
+  {
+    "revision": "17d09b3a0f3c7d23e06c",
+    "url": "/VIA-example/static/js/2.0a11ad4a.chunk.js"
+  },
+  {
+    "revision": "4f30271fbfd9cf066338",
+    "url": "/VIA-example/static/js/main.502f2f17.chunk.js"
+  },
+  {
+    "revision": "7cbbd4db8cdaad090860",
+    "url": "/VIA-example/static/js/runtime-main.6b657626.js"
+  },
+  {
+    "revision": "3ced2c689dfd5415bb3226a81ab1f16c",
+    "url": "/VIA-example/static/media/citizen.3ced2c68.svg"
+  },
+  {
+    "revision": "b4a7bfafa87f3e6f61431a1022dc098e",
+    "url": "/VIA-example/static/media/professional.b4a7bfaf.svg"
+  }
+]);
